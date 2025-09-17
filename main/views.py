506349bce167efccc13f3682773e6b2968a83f5a
jebs-tech <bungasaragih688@gmail.com>
2025-09-17 15:08:12 +0700

    Tutorial 3 Selesai

--- main/views.py
+++ main/views.py
@@ -91,13 +91,12 @@ def login_user(request):
       if form.is_valid():
             user = form.get_user()
             login(request, user)
-            response = HttpResponseRedirect(reverse("main:show_main"))
-            response.set_cookie('last_login', str(datetime.datetime.now()))
-            return response
+            return redirect('main:show_main')
+
    else:
-    form = AuthenticationForm(request)
-    context = {'form': form}
-    return render(request, 'login.html', context)
+      form = AuthenticationForm(request)
+   context = {'form': form}
+   return render(request, 'login.html', context)
 
 def logout_user(request):
     logout(request)
